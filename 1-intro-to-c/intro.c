ef43a7f49d2e866b81a3c2eb3bfd99c2809bd933
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-06-19 19:50:54 -0400

    intro to c starter file

---
+++ 1-intro-to-c/intro.c
@@ -0,0 +1,42 @@
+// Basic C Programming Practice
+
+#include <stdio.h>  // standard input/output library
+#include <stdlib.h> // standard library for memory allocation, process control, etc.
+#include <string.h> // string handling functions
+#include <errno.h>  // error number definitions
+
+#define bufsize 1000 // Define a buffer size for input
+
+/* A buffer is a temporary storage area in memory used to hold data
+while it is being transferred between two locations, such as between
+a program and an input/output device. */
+
+// STRUCTS INTRO
+struct MyStruct
+{
+    char character[50];
+    int number;
+};
+
+typedef struct MyStruct NewStructName; // Typedef for easier reference
+
+int main()
+{
+
+    // to call the struct we just created
+    // this is how we can create an instance of the struct MyStruct
+    NewStructName myStructInstance;
+
+    // to call members of the struct
+    strcpy(myStructInstance.character, "Hello, World!"); // Copy string into character array
+
+    /*The strcpy() function in C is a standard library function used to copy a null-terminated
+    string from a source location to a destination location. It is declared in the <string.h>
+    header file.*/
+
+    myStructInstance.number = 42; // Assign an integer value
+
+    // printing the values of the struct members
+    printf("Character: %s\n", myStructInstance.character); // %s is ua format specifier for strings
+    printf("Number: %d\n", myStructInstance.number);       // %d is a format specifier for integers
+}
