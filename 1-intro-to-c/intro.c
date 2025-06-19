ac4f91e46fb166773152519794a8d6c8e14c8202
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-06-19 19:54:37 -0400

    buffers and define statements

--- 1-intro-to-c/intro.c
+++ 1-intro-to-c/intro.c
@@ -1,15 +1,29 @@
 // Basic C Programming Practice
 
+/*
+Includes are directives that tell the compiler to include the contents of a file in the program.
+*/
+
 #include <stdio.h>  // standard input/output library
 #include <stdlib.h> // standard library for memory allocation, process control, etc.
 #include <string.h> // string handling functions
 #include <errno.h>  // error number definitions
 
+/*
+DEFINE is a preprocessor directive that defines a macro.
+MACRO is a fragment of code that has been given a name. Whenever the name
+is used, it is replaced by the code fragment.
+
+For example: #define SQUARE(x) ((x) * (x))
+*/
+
 #define bufsize 1000 // Define a buffer size for input
 
-/* A buffer is a temporary storage area in memory used to hold data
+/*
+A buffer is a temporary storage area in memory used to hold data
 while it is being transferred between two locations, such as between
-a program and an input/output device. */
+a program and an input/output device.
+*/
 
 // STRUCTS INTRO
 struct MyStruct
