5ce9b5f5c3020408e89715d6f18872883ee29d6f
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-08-06 16:01:09 -0400

    3.1 fork and find

---
+++ Exams/practice-set/3.1-fork-n-find.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*
+Program:
+- linear array of integers: want to perform linear search with three processes
+- e.g. splitting the work between one parent and two children
+- parent process will fork two child processes
+- each child will search a portion of the array for a target value
+- each child will return the index of the target value if found, or -1 if not
+- parent will wait for both children to finish and then print the results
+*/
+
+#define ARRAY_SIZE 100
+#define TARGET_VALUE 42
+
+int array[ARRAY_SIZE]; // global array to be searched
+int array_len = ARRAY_SIZE;
+int search_value = TARGET_VALUE;
+
+// Note: no need for pipes since the processes don't need to communicate
